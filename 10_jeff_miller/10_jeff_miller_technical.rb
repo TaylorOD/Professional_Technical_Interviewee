9ffee085f2742fa8479d9a77ed6a4401031b4363
taylorod <taylorsunwantedemails@gmail.com>
2021-04-24 22:05:39 -0500

    add jeff example solution

--- 10_jeff_miller/10_jeff_miller_technical.rb
+++ 10_jeff_miller/10_jeff_miller_technical.rb
@@ -1,45 +1,5 @@
+# Problem:
+# https://github.com/jeffmiller00/katas/blob/master/monte_hall/Interview%20Monty%20Hall.pdf
 
-# output: percentage of times it was true vs false - 66, 34
-
-class Game
-  def initialize
-    doors_array = [Door.new, Door.new, Door.new_winner].shuffle
-
-    player_choice = :switch #:not_switch
-  end
-
-
-end
-
-class Doors
-  def initialize(what_inside = :goat)
-    @prize = what_inside
-    @state = :closed #:open
-  end
-
-  def self.new_winner
-    Door.new(:car)
-  end
-
-  def winner?
-    @price == :car
-  end
-
-  def loser?
-    !winner?
-  end
-
-  def open?
-    @state == :open
-  end
-
-  def open!
-    raise (AlreadyOpen, "The door is already opened.") if self.open?
-    @state = :open
-    @prize
-  end
-
-  def close?
-    !open?
-  end
-end
+# Solution:
+# https://github.com/jeffmiller00/katas/tree/master/monte_hall

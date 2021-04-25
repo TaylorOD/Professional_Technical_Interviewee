26a4c4e941e570600c7efa21771223a5a6e28571
taylorod <taylorsunwantedemails@gmail.com>
2021-04-24 22:03:22 -0500

    add technical for matt episode

---
+++ 11_matt_karazin/11_matt_karazin.js
@@ -0,0 +1,42 @@
+// Interview Challenge
+// Problem
+// Mega Finance Company offers a finance tracking application to users on the iOS and Android app stores. Users link their bank accounts and the finance tracking application automatically pulls in all the transactions from their bank accounts. The application shows the users the transactions and offers insights and summaries of their spending, such as total spend this month and total spent on groceries.
+
+// A new feature of Mega Finance Company’s app is to allow users to add transactions manually. Users have asked for this feature for two main reasons:
+
+// - To support tracking their finances without linking their bank account
+// - To add transactions that occur outside their bank accounts, such as paying with cash instead of using a bank card
+
+// Your task is to write an MVP specification for this feature.
+
+// Example user scenarios:
+// Tim went out to eat at a sushi restaurant. The bill was $24 and he paid in cash. He now wants to add this transaction to the application.
+
+// Vanessa paid rent, $930, using a check from a bank account she has not linked to the application. She now wants to add this transaction to the application.
+
+// Mishka received a birthday gift, $50 in cash. She wants to add this gift to the application.
+
+// Questions:
+// Data Collected:
+// Please list the data you believe the user should be able to enter when adding a manual transaction.
+
+
+
+// API Endpoint Input:
+// Please document the API endpoint to add a manual transaction. This should include the HTTP method, the URL path, and any parameters.
+
+
+
+
+// API Endpoint Response:
+// Please document any common responses the API may return. This should include the HTTP status code and any data returned.
+
+
+
+// Data Model and Storage:
+// Please document a data model for storing the transaction. You should also indicate what type of data store you would use (MySQL, Postgres, MongoDB, etc).
+
+
+
+// Plan to Accomplish:
+// Please list what steps a software engineer would need to accomplish to complete this feature. These steps could be considered individual tasks that we would track during a sprint.

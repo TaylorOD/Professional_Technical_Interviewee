a5d5fbe69606e4a34e60d586220eefe3f1d08998
taylorod <taylorsunwantedemails@gmail.com>
2021-08-11 07:20:26 -0600

    Add umang technical

---
+++ 17_umang_kapadia/17_umang_kapadia_technical.js
@@ -0,0 +1,33 @@
+// Quality and Efficiency
+
+// Background:
+// Real world problem solving is a complex field with multiple solutions and multiple
+// trade-offs. Rarely are solutions perfect, but rather an optimization for a given perspective.
+// If you google around you can find multiple frameworks for problem solving in relation to
+// a field of work. Example: DMAIC
+
+// For the problem below, you can choose to use any of the multitude of frameworks that
+// exist, or make up your own, but you are not required to use one at all. There isn’t a right
+// answer for the given problem. What does matter is the process by which you’ve drawn
+// your conclusion. Be descriptive as possible on why you are making your choices.
+
+// Setup:
+// Every morning you must make a peanut butter sandwich, fresh apple juice, and crackers
+// with cream cheese for your child’s lunch. Once all items are completed they must be
+// placed in her backpack. You can only start making the items once she is up, you also
+// aren’t sure when she’ll wake up since you aren’t responsible for waking her up and
+// getting her ready. You have unlimited resources such as people to help make items,
+// you’ll never be out of anything needed to make an item, etc. Your goal is to be done as
+// fast as possible, because only then can you sit down and relax, maybe open a beer, it’s 7a,
+// but who cares.
+
+// Challenge:
+// After weeks of doing this you discovered that even though you do the exact same thing
+// every day, your results vary every day. Some days an item doesn’t make it into the
+// backpack, some days you're done super fast, others you're late. Some days all the items
+// make it in the backpack, but they aren’t completed - like a sandwich doesn’t have
+// peanut butter on it, and some days everything is perfect.
+
+// Objective:
+// You’ve decided to start all over and rethink the morning process. Create and describe a
+// new process.
